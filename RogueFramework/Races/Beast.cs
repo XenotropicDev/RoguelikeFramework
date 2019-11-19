215b6a7870f6bbb439d36085a2038dc2641d2d01
XenotropicDev <xenotropicdev@gmail.com>
2019-11-18 22:18:48 -0800

    Staging for pattern based changes

---
+++ RogueFramework/Races/Beast.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using RogueFramework.Base;
+
+namespace RogueFramework.Races
+{
+    class Beast : Base.ICreatureType
+    {
+        public string Name { get => Name; set => Name = value; }
+        public char CreatureIcon { get; set; }
+
+        public Beast()
+        {
+            Name = "Beast";
+            CreatureIcon = 'b';
+        }
+    }
+}
